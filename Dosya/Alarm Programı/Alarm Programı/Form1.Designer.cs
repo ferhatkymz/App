ddac53361fe92ed1b9a086bb605487f9e9b624fb
ferhatkymz <150702933+ferhatkymz@users.noreply.github.com>
2024-12-20 16:52:31 +0300

    create new folder
    Yeni bir işleme tabi tuttum

---
+++ Dosya/Alarm Programı/Alarm Programı/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace Alarm_Programı
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblsa = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lbldk = new System.Windows.Forms.Label();
+            this.lblsn = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.lbluzunsa = new System.Windows.Forms.Label();
+            this.lbluzunta = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(18, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Saat";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(18, 60);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(89, 28);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(128, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 30);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Dakika";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(127, 60);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(89, 28);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(42, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 39);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Alarm Kur";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblsa
+            // 
+            this.lblsa.AutoSize = true;
+            this.lblsa.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.lblsa.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblsa.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.lblsa.Location = new System.Drawing.Point(17, 161);
+            this.lblsa.Name = "lblsa";
+            this.lblsa.Size = new System.Drawing.Size(28, 31);
+            this.lblsa.TabIndex = 6;
+            this.lblsa.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lbldk
+            // 
+            this.lbldk.AutoSize = true;
+            this.lbldk.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.lbldk.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbldk.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.lbldk.Location = new System.Drawing.Point(86, 160);
+            this.lbldk.Name = "lbldk";
+            this.lbldk.Size = new System.Drawing.Size(28, 31);
+            this.lbldk.TabIndex = 7;
+            this.lbldk.Text = "0";
+            // 
+            // lblsn
+            // 
+            this.lblsn.AutoSize = true;
+            this.lblsn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.lblsn.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblsn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.lblsn.Location = new System.Drawing.Point(147, 160);
+            this.lblsn.Name = "lblsn";
+            this.lblsn.Size = new System.Drawing.Size(28, 31);
+            this.lblsn.TabIndex = 8;
+            this.lblsn.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(61, 160);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(22, 31);
+            this.label6.TabIndex = 9;
+            this.label6.Text = ":";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(121, 160);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(22, 31);
+            this.label7.TabIndex = 10;
+            this.label7.Text = ":";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(250, 197);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 39);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Durdur";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(377, 197);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 39);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Tekrar Kur";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // lbluzunsa
+            // 
+            this.lbluzunsa.AutoSize = true;
+            this.lbluzunsa.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.lbluzunsa.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbluzunsa.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.lbluzunsa.Location = new System.Drawing.Point(26, 59);
+            this.lbluzunsa.Name = "lbluzunsa";
+            this.lbluzunsa.Size = new System.Drawing.Size(28, 31);
+            this.lbluzunsa.TabIndex = 13;
+            this.lbluzunsa.Text = "0";
+            // 
+            // lbluzunta
+            // 
+            this.lbluzunta.AutoSize = true;
+            this.lbluzunta.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.lbluzunta.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbluzunta.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.lbluzunta.Location = new System.Drawing.Point(26, 11);
+            this.lbluzunta.Name = "lbluzunta";
+            this.lbluzunta.Size = new System.Drawing.Size(28, 31);
+            this.lbluzunta.TabIndex = 14;
+            this.lbluzunta.Text = "0";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lbluzunta);
+            this.panel1.Controls.Add(this.lbluzunsa);
+            this.panel1.Location = new System.Drawing.Point(202, 259);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(333, 100);
+            this.panel1.TabIndex = 15;
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(231, 12);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(304, 179);
+            this.axWindowsMediaPlayer1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(542, 456);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblsn);
+            this.Controls.Add(this.lbldk);
+            this.Controls.Add(this.lblsa);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblsa;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lbldk;
+        private System.Windows.Forms.Label lblsn;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label lbluzunsa;
+        private System.Windows.Forms.Label lbluzunta;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Dosya/Alarm Programı/Alarm Programı/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Alarm_Programı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button2.Visible = false;
+            button3.Visible = false;
+            timer1.Enabled = true;
+            for(int i = 0; i <= 23; i++)
+            {
+                comboBox1.Items.Add(i);
+            }
+            for(int j = 00; j <= 59; j++)
+            {
+                comboBox2.Items.Add(j);
+            }
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            timer1.Enabled = true;
+            button1.Visible = false;
+            comboBox1.Enabled = false;
+            comboBox2.Enabled = false;
+            button3.Visible = true;
+            
+           
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblsa.Text = DateTime.Now.Hour.ToString();
+            lbldk.Text = DateTime.Now.Minute.ToString();
+            lblsn.Text = DateTime.Now.Second.ToString();
+            lbluzunta.Text = DateTime.Now.ToLongDateString();//uzun tarih
+            lbluzunsa.Text = DateTime.Now.ToLongTimeString();//uzunsaat
+
+            if (comboBox1.Text == lblsa.Text && comboBox2.Text == lbldk.Text)
+            {
+                timer1.Enabled = false;
+                
+                axWindowsMediaPlayer1.URL = "C:\\Users\\Ferhat\\source\\repos\\Alarm Programı\\Alarm Programı\\bin\\Debug\\1.mp3";
+                button2.Enabled = true;
+                MessageBox.Show("Uyanma Zamanı!!!!");
+                button2.Visible = true;
+                comboBox1.Text = "";
+                comboBox2.Text = "";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Visible = false;
+            axWindowsMediaPlayer1.Ctlcontrols.stop();
+            timer1.Enabled = true;
+            button1.Visible = true;
+            comboBox1.Enabled = true;
+            comboBox2.Enabled = true;
+    }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            comboBox1.Text = "";
+            comboBox2.Text="";
+            button1.Visible = true;
+            comboBox1.Enabled = true;
+            comboBox2.Enabled = true;
+            button3.Visible = false;
+
+        }
+    }
+}
